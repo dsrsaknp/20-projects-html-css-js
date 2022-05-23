69f7902a70711f85727e9e2a61aeccb7e6bb0e30
divyasharma <divya.sharma1@publicissapient.com>
2022-05-24 01:07:09 +0530

    Infinte scroll initial commit

---
+++ infinite-scroll/script.js
@@ -0,0 +1,89 @@
+// DOM elements
+const imageContainerElement = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+// global variables
+const api_access_key = 'O4q5fFLVNZZR3m0I1K_z5-GXMxW4WOYyWHv1e7hvQqo';
+const api_secret_key = 'VcUQLfuUeVbruxCbCsAZtZbW0Ji6AlE5WrHNTTPLOfQ';
+let count = 5;
+let apiUrl = `https://api.unsplash.com/photos/random?client_id=${api_access_key}&count=${count}`;
+
+let photosArray = [];
+let imageLoads = 0;
+let loadMoreImages = false;
+let totalImagesLoaded = 0;
+
+// Helper function to set attributes to elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Images loaded
+function imagesLoaded() {
+    imageLoads++;
+    if (imageLoads === totalImagesLoaded) {
+        loader.hidden = true;
+        loadMoreImages = true;
+        count = 10;
+        apiUrl = `https://api.unsplash.com/photos/random?client_id=${api_access_key}&count=${count}`;
+    }
+}
+
+// Create element for Link and Photos, add to DOM 
+function displayPhotos() {
+    imageLoads = 0;
+    totalImagesLoaded = photosArray.length;
+    console.log('totalImagesLoaded ', totalImagesLoaded);
+    // Run function for each object in photosArray
+    photosArray.forEach(photo => {
+
+        // Create <a> tag to link to Unsplash
+        const a = document.createElement('a');
+        setAttributes(a, {
+            href: 'photo.links.html',
+            target: '_blank'
+        });
+
+        // Create <img> tag
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.description,
+            title: photo.description
+        });
+
+        // Count number of image loads
+        img.addEventListener('load', imagesLoaded);
+
+        // Append <img> -> <a> -> image-container Element
+        a.appendChild(img);
+        imageContainerElement.appendChild(a);
+    })
+}
+
+// Get photos from Unsplash API
+async function getRandomPhotosFromAPI() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    }
+    catch (error) {
+        console.log(`Whoops! something went wrong. Please try again!`);
+    }
+}
+
+// Load more images
+function loadMore() {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && loadMoreImages) {
+        loadMoreImages = false;
+        loader.hidden = false;
+        getRandomPhotosFromAPI();
+    }
+}
+window.addEventListener('scroll', loadMore);
+
+// invoked API function
+getRandomPhotosFromAPI();
